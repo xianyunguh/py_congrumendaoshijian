d39c0a27727af20bb0b58da78bed86a634d51b49
xianyunguh <hao121@gmail.com>
2018-02-22 21:50:50 +0800

    # 4.1.3 在 for 循环结束后执行一些操作

--- 008_magicians.py
+++ 008_magicians.py
@@ -10,6 +10,12 @@ for magician in magicians:
 
 # 4.1.2 在 for 循环中执行更多的操作 page 46
 
+magicians = ['alice','david','carolina']
+for magician in magicians:
+    print(magician.title() + ', that was a great trick!')
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+# 4.1.3 在 for 循环结束后执行一些操作 page 47
 
 
 
