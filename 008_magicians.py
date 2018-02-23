263574883620602d4254575961a9bb92a976f636
xianyunguh <hao121@gmail.com>
2018-02-23 09:45:04 +0800

    # 4.2.5 遗漏了冒号 page 50

--- 008_magicians.py
+++ 008_magicians.py
@@ -17,6 +17,33 @@ for magician in magicians:
 
 # 4.1.3 在 for 循环结束后执行一些操作 page 47
 
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+# 4.2 避免缩进错误 page 47
+
+# 4.2.1 忘记缩进 page 48
+
+# 4.2.2 忘记缩进额外的代码行 page 48
+
+# 4.2.3 不必要的缩进 page 49
+
+# 4.2.4 循环后不必要的缩进 page 49
+
+# 4.2.5 遗漏了冒号 page 50
+
+
+
+
+
+
+
+
+
 
 
 
