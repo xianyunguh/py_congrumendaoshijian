700a07dc70dd386a64bef7a628c184c1d1b84d58
xianyunguh <hao121@gmail.com>
2018-02-24 21:09:12 +0800

    a

--- 019_voting.py
+++ 019_voting.py
@@ -9,3 +9,13 @@ if age >= 18:
 
 # 5.3.2 if-else 语句 page 70
 
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+
+
---
+++ 020_amusement_park.py
@@ -0,0 +1,73 @@
+# 5.3.3 if-elif-else 结构 page 72
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $5.")
+else:
+    print("Your admission cost is $10.")
+
+# 上例改进
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
+
+# 5.3.4 使用多个 elif 代码块 page 73
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
+
+# 5.3.5 省略 else 代码块 page 74
+
+# Python并不要求if-elif结构后面必须有else代码块。
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
+
+# 5.3.6 测试多个条件 page 74
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+
+
+
+
+
